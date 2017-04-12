018eaa609fc2975e45dd78c0d484fb47c46c77d0
Adam Clarke <adam.clarke@thefilter.com>
2017-04-12 16:50:08 +0100

    Introduce explicit JsonConverter to comply with strict_date_format defined in mappings This change assumes direct control of ElasticClient generation in order to introduce a new IsoDateTimeConverter which forces dates to match the format yyyy-MM-ddTHH:mm:ss.fffZ. Without this change, dates are serialized like so: 2017-04-06T11:25:35.6444179+00:00 This does not comply with the strict_date_time format which is attributed on to DateTimeOffset properties in ElasticUserIdentity.cs. I have also extracted the client generation logic out in to a factory which is now also used by the tests to generate an ElasticClient.

---
+++ src/AspNetCore.Identity.Elastic/ElasticClientFactory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using Nest;
+using Elasticsearch.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+[assembly: InternalsVisibleTo("AspNetCore.Identity.Elastic.Tests")]
+namespace AspNetCore.Identity.Elastic
+{    
+    internal static class ElasticClientFactory
+    {
+        public static IElasticClient Create(Uri node, string defaultIndex)
+        {
+            var settings = new ConnectionSettings(
+                new SingleNodeConnectionPool(node),
+                new HttpConnection(),
+                new SerializerFactory(DefineSerializationSettings));
+
+            settings.MapDefaultTypeIndices(m => m
+                .Add(typeof(ElasticIdentityUser), defaultIndex));
+
+            var transport = new Transport<IConnectionSettingsValues>(settings);
+            
+            return new ElasticClient(settings);
+        }
+
+        private static void DefineSerializationSettings(
+            JsonSerializerSettings jsonSerializerSettings,
+            IConnectionSettingsValues connectionSettingsValues)
+        {
+            //Since we use strict_date_time format in our elasticsearch mappings for all dates, we 
+            //need to ensure that serialized dates match that format. The format defined below 
+            //is based on strict_date_time in elasticsearch version 5.3.0. 
+            //documentation @ https://www.elastic.co/guide/en/elasticsearch/reference/5.3/mapping-date-format.html
+            var dateTimeConverter = new IsoDateTimeConverter
+            {
+                DateTimeFormat = "yyyy-MM-ddTHH:mm:ss.fffZ",
+                DateTimeStyles = DateTimeStyles.AdjustToUniversal
+            };
+
+            if (jsonSerializerSettings.Converters == null)
+            {
+                jsonSerializerSettings.Converters = new List<JsonConverter>();
+            }
+
+            jsonSerializerSettings.Converters.Add(dateTimeConverter);
+        }
+    }
+}
